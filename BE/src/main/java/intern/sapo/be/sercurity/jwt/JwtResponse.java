d1ec482275e63b3383b11bd825ad0bdf404cea1a
nguyenducanh0207 <anhnguyen02072000@gmail.com>
2022-08-15 11:03:48 +0700

    update security

---
+++ BE/src/main/java/intern/sapo/be/sercurity/jwt/JwtResponse.java
@@ -0,0 +1,15 @@
+package intern.sapo.be.sercurity.jwt;
+import java.io.Serializable;
+
+
+public class JwtResponse implements Serializable {
+    private final String jwttoken;
+
+    public JwtResponse(String jwttoken) {
+        this.jwttoken = jwttoken;
+    }
+
+    public String getToken() {
+        return this.jwttoken;
+    }
+}
