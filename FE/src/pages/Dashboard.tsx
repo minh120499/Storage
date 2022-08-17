2ae00fce851ecc1a4fc0cdaeb63bd78682fc8e3e
minhvh <minhfof@gmail.com>
2022-08-17 15:52:03 +0700

    ok

---
+++ FE/src/pages/Dashboard.tsx
@@ -0,0 +1,43 @@
+import SideBar from "../components/SideBar";
+import HeaderMenu from "../components/Header";
+
+import { Layout } from "antd";
+import React, { useState } from "react";
+import { Outlet } from "react-router";
+const { Sider, Content, Header } = Layout;
+
+const Dashboard: React.FC = () => {
+  const [collapsed, setCollapsed] = useState(false);
+
+  return (
+    <Layout style={{minHeight: "100vh"}}>
+      <Sider width="auto">
+        <SideBar />
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="top-header" style={{ padding: 0 }}>
+          {/* {React.createElement(
+            collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+            {
+              className: "trigger",
+              onClick: () => setCollapsed(!collapsed),
+            }
+          )} */}
+          <HeaderMenu />
+        </Header>
+        <Content
+          className="site-layout-background"
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280,
+          }}
+        >
+          <Outlet />
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default Dashboard;
