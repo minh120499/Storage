8d69e120177fac9c04778032111d6e92c50111a1
xgamehades <ninhducthien2k@gmail.com>
2022-08-26 22:52:39 +0700

    update_Product

--- FE/src/pages/product/ListProduct.tsx
+++ FE/src/pages/product/ListProduct.tsx
@@ -3,7 +3,7 @@ import * as Antd from 'antd'
 import * as Mui from '@mui/material'
 
 
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import { ISupplier, TypeSupplier } from "../../services/customType";
 
 
@@ -13,7 +13,9 @@ import "../../styles/Table.css";
 import { IProductCount, IProductFilter } from '../../type/allType';
 import { countProductByFilter, getProducts } from '../../services/productServices';
 import ProductPagination from './ProductPagination';
-import { PlusOutlined } from '@ant-design/icons';
+import { DeleteOutlined, DownOutlined, PlusOutlined, SortDescendingOutlined, StopOutlined,DownloadOutlined } from '@ant-design/icons';
+import { Sort } from '@mui/icons-material';
+import { margin } from '@mui/system';
 const initFilter: IProductFilter = {
     key: '',
     isDelete: false,
@@ -52,112 +54,86 @@ const ProductCol = [
         }
     }
 ]
+const menu = (
+    <Antd.Menu
+        items={[
+            {
+                label: <Link to="#">Ngừng giao dịch</Link>,
+                key: '1',
+                icon: <StopOutlined />,
+            },
 
+            {
+                label: <Link to="#">Xóa nhà cung cấp</Link>,
+                key: '2',
+
+                icon: <DeleteOutlined />,
+            }
+        ]}
+    />
+);
 
 
 const ListProduct = () => {
-    var keyWord=''
-
+    var keyWord = ''
     const [productFilter, setProductFilter] = useState<IProductFilter>(initFilter)
     const [products, setProducts] = useState<Array<IProductCount>>([])
     const [reload, setReload] = useState(false)
     const navigate = useNavigate()
     const [totalPage, setTotalPage] = useState<number>(1);
     const [selectProduct, setSelectProduct] = useState<React.Key[]>([]);
-    const onSelectChange = (newSelectedRowKeys: React.Key[]) => {
-        setSelectProduct(newSelectedRowKeys);
-    };
 
-    const rowSelection = {
-        selectProduct,
-        onChange: onSelectChange,
-    };
-
-// <<<<<<< HEAD
-//     }, [productFilter])
-//     useEffect(() => {
-//         countProductByFilter(productFilter).then((response) => response.json())
-//             .then((res) => {
-//                 setTotalPage(res)
-//             }).catch(error=>{
-//                 console.log(error);     
-//             })
-
-//     }, [])
-// =======
-// >>>>>>> 5628737534f51c8c5596d8e7c15566fc672ddc3f
-
-    let hasSelected = selectProduct.length > 0;
-    let hasOneSelected = selectProduct.length === 1;
-    const handleChangePage = (event: React.MouseEvent<HTMLButtonElement> | null, page: number) => {
-        setProductFilter({ ...productFilter, page: page })
-
-    }
-    const handleChangeRowsPerPage = (
-        event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
-    ) => {
-
-        setProductFilter({ ...productFilter, size: Number(event.target.value), page: 1 })
-
-    };
+    const [isOpenFilter, setIsOpenFilter] = useState(false)
 
 
-    useEffect(() => {
-        
+    useLayoutEffect(() => {
+
         getProducts(productFilter).then((response) => response.json())
             .then((res) => {
                 setProducts(res)
 
-            }).catch(error=>{})
-            countProductByFilter(productFilter).then((response) => response.json())
+            }).catch(error => { })
+        countProductByFilter(productFilter).then((response) => response.json())
             .then((res) => {
                 setTotalPage(res)
-            }).catch(error=>{
+            }).catch(error => {
 
-                
+
             })
 
     }, [productFilter])
-   
+
+    const onPageChange = (page: number, size: number) => {
+        setProductFilter({ ...productFilter, page: page, size: size })
+
+    }
+
+    const showFilter = () => {
+        setIsOpenFilter(true)
+    }
+    const closeFilter = () => {
+        setIsOpenFilter(false)
+    }
     const Products = () => {
         return (
             <>
 
                 <Antd.Table dataSource={products}
-                    scroll={{ x: 500, y: 500 }}
+                    sticky
                     columns={ProductCol}
                     rowKey="id"
-                    bordered
                     pagination={false}
-
+                    style={{ height: 550, maxHeight: 550, overflow: 'scroll' }}
                     onRow={(record) => {
                         return {
                             onClick: event => navigate({ pathname: `/products/${record.id}` }),
                         }
                     }}
-                    rowSelection={rowSelection}
+                    rowSelection={undefined}
 
 
                 />
 
-                <Mui.TablePagination
-                    rowsPerPageOptions={[10, 25, 50, 100, 200]}
-                    count={totalPage}
-                    rowsPerPage={productFilter.size}
-                    page={productFilter.page}
-                    labelDisplayedRows={() => {
-                        return (
-                            <>
-                                <Antd.InputNumber  value={productFilter.page} ></Antd.InputNumber> /{Math.ceil(totalPage / productFilter.size)}
-                            </>
-                        )
-                    }}
-                    component='div'
-                    onPageChange={handleChangePage}
-                    onRowsPerPageChange={handleChangeRowsPerPage}
-                    ActionsComponent={ProductPagination}
-                    style={{ verticalAlign: "center" }}
-                />
             </>
         )
     }
@@ -165,40 +141,74 @@ const ListProduct = () => {
     return (
         <>
             <Antd.Typography.Title>Danh sách sản phẩm</Antd.Typography.Title>
+            <Antd.Drawer title="Tìm kiếm nâng cao" placement="right" onClose={closeFilter} visible={isOpenFilter}>
 
+            </Antd.Drawer>
             <Mui.Grid container spacing={2} sx={{ mb: 2 }}>
-                <Mui.Grid item xs={2}>
-                    <Antd.Input  >
-                    </Antd.Input>
+                <Mui.Grid item xs={1.5}>
+                    <Antd.Dropdown overlay={menu} >
+                        <Antd.Button style={{ width: "100%", fontSize: '14px' ,margin:0}} type="primary">
+                            <Antd.Space>
+                                Thao tác
+                                <DownOutlined />
+                            </Antd.Space>
+                        </Antd.Button>
+                    </Antd.Dropdown>
 
                 </Mui.Grid>
-                <Mui.Grid item xs={8}>
+                <Mui.Grid item xs={5.5} sx={{m:0}}>
                     <Antd.Input
                         placeholder='Nhập tên hoặc mã sản phẩm'
-                       onChange={(e)=>{
-                        keyWord=e.target.value.toString()
+                        onChange={(e) => {
+                            keyWord = e.target.value.toString()
 
-                       }}
-                        onKeyDown={(e)=>{
-                            if(e.key==='Enter')
-                           setProductFilter({...productFilter,key:keyWord})
                         }}
-                        
+                        onKeyDown={(e) => {
+                            if (e.key === 'Enter')
+                                setProductFilter({ ...productFilter, key: keyWord, page: 1 })
+                        }}
+
                     >
 
                     </Antd.Input>
                 </Mui.Grid>
+                <Mui.Grid item xs={2} sx={{ display: 'flex', justifyContent: 'center' }}>
+
+                    <Antd.Button onClick={showFilter} style={{ width: "100%", margin: 0 }} type='primary'>
+                        <Antd.Space>
+                            <SortDescendingOutlined />
+                            Tìm kiếm nâng cao
+                        </Antd.Space>
+                    </Antd.Button>
+                </Mui.Grid>
+                <Mui.Grid item xs={1}>
+                    <Antd.Button style={{ width: "100%", fontSize: '14px', margin: 0 }} type="primary" >
+                        <Antd.Space>
+                            <DownloadOutlined />
+                            Xuất file
+                        </Antd.Space>
+                    </Antd.Button>
+                </Mui.Grid>
                 <Mui.Grid item xs={2}>
-                    <Antd.Button style={{ width: "100%", fontSize: '14px', margin: 0 }} type="primary">
+                    <Antd.Button style={{ width: "100%", fontSize: '14px', margin: 0 }} type="primary" onClick={() => { navigate('/products') }} >
                         <Antd.Space>
                             <PlusOutlined />
                             Thêm mới
                         </Antd.Space>
                     </Antd.Button>
                 </Mui.Grid>
-                <Mui.Grid item xs={12}>
-                    <Products />
+
+                <Mui.Grid item xs={12} >
+                    <Mui.Paper sx={{ pb: 2 }} >
+                        <Products />
+                        <div style={{ display: 'flex', justifyContent: 'end' }}>
+                            <Antd.Pagination responsive style={{ marginTop: 10, marginRight: 10 }} showQuickJumper defaultCurrent={1} total={totalPage} onChange={onPageChange} pageSizeOptions={[5, 10, 20, 50, 100]} />
+
+                        </div>
+
+                    </Mui.Paper>
                 </Mui.Grid>
+
             </Mui.Grid>
 
 
