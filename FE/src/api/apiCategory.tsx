15ce25c64e4af712e633bd7de875d2a5290f26e1
nguyenducanh0207 <anhnguyen02072000@gmail.com>
2022-08-27 12:44:27 +0700

    add_api_category

---
+++ FE/src/api/apiCategory.tsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export const getCategories = async () => { 
+    return await axios.get(`http://localhost:8080/api/categories`)
+}
+
+export const createCategory = async (category:object)=>{
+    return axios.post(`http://localhost:8080/api/categories/category`,category)
+}
+
+export const deleteListCategory = async (listId: React.Key[])=>{
+    return axios.post("http://localhost:8080/api/categories/delete", listId)
+}
+
+export const updateCategory = async (category:object, idUpdate:number)=>{
+    return axios.put(`http://localhost:8080/api/categories/category/${idUpdate}`,category)
+}
+
+export const deleteCategory = async (id: number)=>{
+    return axios.delete(`http://localhost:8080/api/categories/delete/${id}`)
+}
