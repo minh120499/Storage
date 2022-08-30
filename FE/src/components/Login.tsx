ff0aa72521b3a4fa86c2d20947a7f4cde2aaf947
minhvh <minhfof@gmail.com>
2022-08-30 18:49:35 +0700

    nothing

--- FE/src/components/Login.tsx
+++ FE/src/components/Login.tsx
@@ -1,14 +1,16 @@
 import { useMutation } from "@tanstack/react-query";
-import { Form, Input,Spin  } from "antd";
+import { Form, Image, Input, Spin } from "antd";
 import axios from "axios";
 // import Swal from "sweetalert2";
 import { ILoginData } from "../interface";
-import Button from '../UI/Button'
+import Button from "../UI/Button";
+import PersonIcon from "@mui/icons-material/Person";
+import VpnKeyIcon from "@mui/icons-material/VpnKey";
 
 const Login: React.FC = () => {
   const [loginForm] = Form.useForm();
-  const loginSubmit = useMutation((loginData: ILoginData) => {
-    return axios.post("http://localhost:8080/api/account/login", loginData);
+  const loginSubmit = useMutation( async (loginData: ILoginData) => {
+    return (await axios.post("http://localhost:8080/api/account/login", loginData)).data;
   });
 
   const onFinishFailed = (errorInfo: any) => {
@@ -17,65 +19,84 @@ const Login: React.FC = () => {
 
   const onFinish = async (values: any) => {
     const { username, password } = loginForm.getFieldsValue();
-    console.log(username, password);
-
     loginSubmit.mutate({ username, password });
-
-    console.log(loginSubmit.isLoading);
-
   };
+
+  if(loginSubmit?.data) {
+    console.log(loginSubmit.data);
+  }
+  
 
   return (
     <>
-    <div
-      className=" flex justify-center items-center h-screen w-screen"
-      style={{
-        backgroundImage: "url('/loginBg.svg')",
-        backgroundSize: "cover",
-      }}
-    >
-      {loginSubmit.isLoading ? <Spin /> : loginSubmit.isError ? 'flas' : loginSubmit.data ? 'loginSubmit.data' : ''}
-      <Form
-        className="relative h-max w-max p-10 rounded-md"
-        form={loginForm}
-        labelCol={{ span: 8 }}
-        wrapperCol={{ span: 16 }}
-        onFinish={onFinish}
-        onFinishFailed={onFinishFailed}
-        autoComplete="off"
+      <div
+        className=" flex justify-center items-center h-screen w-screen gap-5 shadow-black"
+        style={{ background: "#f0f8ff" }}
       >
-        <Form.Item
-          label="Username"
-          name="username"
-          rules={[{ required: true, message: "Please input your username!" }]}
-          initialValue="adminn"
+        {/* {loginSubmit.isLoading ? (
+          <Spin />
+        ) : loginSubmit.isError ? (
+          "flas"
+        ) : loginSubmit.data ? (
+          "loginSubmit.data"
+        ) : (
+          ""
+        )} */}
+        <div className=" shadow-md">
+          <Image
+            className="w-max rounded-xl "
+            style={{ height: 413, objectFit: "contain" }}
+            src="https://img.freepik.com/free-vector/logistic-workers-carrying-boxes-with-loaders_74855-6541.jpg?w=900&t=st=1661854951~exp=1661855551~hmac=6de857e7e90e83f15098dbe800212e54cb9c8f448e246ef4c81ebe22ee7f2a8d"
+          />{" "}
+        </div>
+        <Form
+          className="relative h-max w-max p-10 self-center bg-white rounded-3xl  shadow-md"
+          form={loginForm}
+          labelCol={{ span: 8 }}
+          wrapperCol={{ span: 30 }}
+          onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
+          autoComplete="off"
         >
-          <Input />
-        </Form.Item>
+          <Form.Item>
+            <h2>Đăng nhập</h2>
+          </Form.Item>
 
-        <Form.Item
-          label="Password"
-          name="password"
-          rules={[{ required: true, message: "Please input your password!" }]}
-          initialValue="admin"
-        >
-          <Input.Password />
-        </Form.Item>
-
-        <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-          <Button
-            // style={{
-            //   background:
-            //     "radial-gradient(100% 1111.11% at 100% 51.11%,#00d8b0 0,#00de92 100%)",
-            // }}
-            type="primary"
-            htmlType="submit"
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: "Hãy nhập tài khoản!" }]}
+            initialValue="minh"
           >
-            Login
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
+            <Input
+              className="pl-3"
+              prefix={<PersonIcon style={{ color: "#1890ff" }} />}
+              placeholder="Nhập user name"
+              title="Nhập user name"
+            />
+          </Form.Item>
+
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: "Hãy nhập mật khẩu!" }]}
+            initialValue="minh"
+          >
+            <Input.Password
+              className="pl-3"
+              prefix={<VpnKeyIcon style={{ color: "#1890ff" }} />}
+              placeholder="Nhập password"
+              title="Nhập password"
+            />
+          </Form.Item>
+
+          <Form.Item
+          // wrapperCol={{ span: 30 }}
+          >
+            <Button loading={loginSubmit?.isLoading} type="primary" htmlType="submit" className="w-full m-0">
+              Đăng nhập
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
     </>
   );
 };
